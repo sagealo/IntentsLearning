128faf0409567f67c3363564066fe9a035d1a256
Sage <sagepierone24@gmail.com>
2018-09-25 09:35:16 -0700

    Added Share button

--- app/src/main/java/com/example/per2/intentslearning/SendMessageActivity.java
+++ app/src/main/java/com/example/per2/intentslearning/SendMessageActivity.java
@@ -10,6 +10,7 @@ import android.widget.EditText;
 
 public class SendMessageActivity extends AppCompatActivity {
     private Button buttonSendMessage;
+    private Button buttonShare;
     private EditText editTextMessage;
 
     public static final String EXTRA_SENT_MESSAGE = "the message";
@@ -33,10 +34,25 @@ public class SendMessageActivity extends AppCompatActivity {
                 startActivity(intentSendMessage);
             }
         });
+        buttonShare.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //make intent with desired action
+                Intent intentShareMessage = new Intent(Intent.ACTION_SEND);
+                //set the data type of the stuff we are packaging away
+                intentShareMessage.setType("text/plain");
+                //put the extra with the message
+                intentShareMessage.putExtra(EXTRA_SENT_MESSAGE,
+                        editTextMessage.getText().toString());
+                //launch the activity
+                startActivity(intentShareMessage);
+            }
+        });
     }
 
     private void wireWidgets() {
         buttonSendMessage = findViewById(R.id.button_sendmessage_send);
         editTextMessage = findViewById(R.id.edittext_sendmessage_msg);
+        buttonShare = findViewById(R.id.button_sendmessage_share);
     }
 }
